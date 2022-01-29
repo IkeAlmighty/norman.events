2e71efe750e76a7014652d7295d3d2319b8a2a4a
IkeAlmighty <IsaacYates7@gmail.com>
2022-01-29 12:18:38 -0600

    added meta tag for viewport scaling

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
