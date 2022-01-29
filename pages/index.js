091fbafe8c9bfca4debe77b596c3980070ba87dd
IkeAlmighty <IsaacYates7@gmail.com>
2022-01-29 12:20:00 -0600

    added some test event cards, for now

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,18 @@ export default function Home() {
       </Head>
 
       <main className="px-auto">
+        <div className={styles.navContainer}>
+          <nav className="container max-w-600px">
+            <ul className="list-unstyled row">
+              <li className="text-center d-inline-block border border-dark rounded p-2 bg-light col">
+                <a href="/request">Request and Event</a>
+              </li>
+              <li className="text-center d-inline-block p-2 col">
+                Norman Events
+              </li>
+            </ul>
+          </nav>
+        </div>
         <div className="max-w-600px mx-auto">
           <EventCard
             title="Test Event"
@@ -19,6 +31,31 @@ export default function Home() {
             imgUrl="https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg"
             details="This is a test event"
             eventSlug="testSlug"
+            googleMapUrl={
+              "https://www.google.com/maps?q=norman+ok&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjByPeOs6j1AhXWk2oFHTxGBKcQ_AUoAXoECAIQAw"
+            }
+          />
+
+          <EventCard
+            title="Test Event"
+            entryFee={5.25}
+            imgUrl="https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg"
+            details="This is a test event"
+            eventSlug="testSlug"
+            googleMapUrl={
+              "https://www.google.com/maps?q=norman+ok&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjByPeOs6j1AhXWk2oFHTxGBKcQ_AUoAXoECAIQAw"
+            }
+          />
+
+          <EventCard
+            title="Test Event"
+            entryFee={5.25}
+            imgUrl="https://discovery.sndimg.com/content/dam/images/discovery/fullset/2021/4/30/GettyImages-1189192456.jpg.rend.hgtvcom.406.406.suffix/1619849704543.jpeg"
+            details="This is a test event"
+            eventSlug="testSlug"
+            googleMapUrl={
+              "https://www.google.com/maps?q=norman+ok&um=1&ie=UTF-8&sa=X&ved=2ahUKEwjByPeOs6j1AhXWk2oFHTxGBKcQ_AUoAXoECAIQAw"
+            }
           />
         </div>
       </main>
