a1371ae361313190b4030233cf1ad51c290575da
ikealmighty <IsaacYates7@gmail.com>
2022-02-09 14:45:30 -0600

    fixed render bug where leading 0 in time didn't show up

--- components/EventCard.js
+++ components/EventCard.js
@@ -23,6 +23,7 @@ export default function EventCard({
     const date = new Date(time);
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    if (minutes < 10) minutes = `0${minutes}`; // add leading 0 if it is missing!
     let pmAm = hours > 12 ? "pm" : "am";
     return `${hours % 12}:${minutes}${pmAm}`;
   }
