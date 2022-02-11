19fef9ab6ee05a70dc2a29ef8759bda648ad0ece
ikealmighty <IsaacYates7@gmail.com>
2022-02-11 13:41:30 -0600

    bugfix: 12:xxam and 12:xxpm show up correctly (instead of 0:xx)

--- utils/datetime.js
+++ utils/datetime.js
@@ -5,9 +5,21 @@ export function prettifyDate(time) {
 export function prettifyTime(time) {
   if (!time) return "Invalid Time";
   const date = new Date(time);
+
   let hours = date.getHours();
+
+  // determine pmAm *before* converting hours from military time,
+  // otherwise it won't be accurate:
+  let pmAm = hours > 12 ? "pm" : "am";
+  if (hours === 12) pmAm = "pm";
+  if (hours === 0) pmAm = "am";
+
+  // convert hours to from military time to uh *other* time
+  if (hours !== 12) hours = hours % 12;
+  if (hours === 0) hours = 12;
+
   let minutes = date.getMinutes();
   if (minutes < 10) minutes = `0${minutes}`; // add leading 0 if it is missing!
-  let pmAm = hours > 12 ? "pm" : "am";
-  return `${hours % 12}:${minutes}${pmAm}`;
+
+  return `${hours}:${minutes}${pmAm}`;
 }
